d508a252215f14dfc1933ef38342d4f5d635c000
FAT <FAT@dse.local>
2021-08-12 19:17:16 +0200

    Update to

--- hunt_the_wumpus.py
+++ hunt_the_wumpus.py
@@ -61,8 +61,10 @@ def print_location(player_location):
     if wumpus_location in caves[player_location]:
         print("I smell a wumpus")
 
+"""
+This function is replaced with the ask_for_cave function
 def get_next_location():
-    """ Get the players next location """
+    ### Get the players next location ###
     player_input = input(">")
     if not player_input.isdigit() or int(player_input) not in caves[player_location]:
         print(player_input, " ?")
@@ -70,6 +72,52 @@ def get_next_location():
         return None
     else:
         return int(player_input)
+"""
+
+def ask_for_cave():
+    """ Ask the player to choose a cave from their current location """
+    player_input = input("Which cave?")
+    if not player_input.isdigit() or int(player_input) not in caves[player_location]:
+        print(player_input, " ?")
+        print("That is not a direction i can see")
+        return None
+    else:
+        return int(player_input)
+
+def get_action():
+    """ Find out what the player would like to do next """
+    print("What do you do next")
+    print("m) move")
+    print("a) Fire an arrow")
+    action = input(">")
+    if action == "m" or action == "a":
+        return(action)
+    else:
+        print(action,"?"," That is not an action you dumb fool")
+        return None
+
+def do_movement():
+    """ Doing a movement """
+    print("Moving...")
+    new_location = ask_for_cave()
+    if new_location is None:
+        return player_location
+    else:
+        return new_location
+
+def do_shooting():
+    print("Firing...")
+    shoot_at = ask_for_cave()
+    if shoot_at is None:
+        return False
+
+    if shoot_at == wumpus_location:
+        print("Twang ... You shot the Wumpus!")
+        print("Congratulations dick head <3")
+    else:
+        print("Twang .. clatter, clatter!")
+        print("You wasted your arrow")
+    return True
 
 # Main game
 
@@ -84,15 +132,34 @@ finish_caves()
 print_caves()
 
 wumpus_location = choice(cave_numbers)
+print(wumpus_location)
 player_location = choice(cave_numbers)
 while player_location == wumpus_location:
     player_location = choice(cave_numbers)
 
+while 1:
+    print_location(player_location)
+
+    action = get_action()
+
+    if action == "m":
+        player_location = do_movement()
+        if player_location == wumpus_location:
+            print("Aargh! You got eaten by a wumpus!")
+            break
+
+    if action == "a":
+        game_over = do_shooting()
+        if game_over:
+            break
+
+"""
 while True:
     print_location(player_location)
     new_location = get_next_location()
     if new_location != wumpus_location:
         player_location = new_location
-    if player_location == wumpus_location:
+    if new_location == wumpus_location:
         print("Arghh you got eaten by a wumpus!")
         break
+"""
